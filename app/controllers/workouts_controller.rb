2c899f61a6dd769be0f49364a2e752c86808b45f
Angela <angelalamluu@gmail.com>
2021-03-01 15:21:37 -0800

    successful patch route for edit form

--- app/controllers/workouts_controller.rb
+++ app/controllers/workouts_controller.rb
@@ -11,7 +11,7 @@ class WorkoutsController < ApplicationController
     
     get '/log/:id' do
         #READ A WORKOUT
-        @workout = Workout.find_by(:id => params[:id])
+        find_log
         erb :'workouts/show'
     end
 
@@ -28,18 +28,62 @@ class WorkoutsController < ApplicationController
 
     get '/log/:id/edit' do
         #EDIT A WORKOUT
-        @workout = Workout.find_by(:id => params[:id])
-        @exercises = Exercise.all.select {|exercise| exercise.workout_id == @workout.id}
+        find_log
+        @exercises = @workout.exercises
+        # @exercises = Exercise.all.select {|exercise| exercise.workout_id == @workout.id}
         erb :'workouts/edit'
     end
 
     patch '/log/:id' do
+        if logged_in?
+            find_log
+            if @workout && @workout.user == current_user
+                unless params[:workout][:date].empty?
+                    @workout.update(:date => params[:workout][:date])
+                end
+
+                @workout.exercises.clear
+                @exercises = exercise_names.zip(exercise_weights)
+                @exercises.each do |exercise|
+                    @workout.exercises << Exercise.create(:workout_id => @workout.id, :name => exercise[0], :weight => exercise[1])
+                end
+            
+                # unless params[:exercise][:name].empty? && params[:exercise][:name].empty?
+                #     names = params[:workout][:exercise][:name].reject {|exercise| exercise.empty?}
+                #     weights = params[:workout][:exercise][:weight].reject {|weight| weight.empty?}
+                #     @exercises = names.zip(weights)
+                #     @exercises.each do |exercise|
+                #         @workout.exercises << Exercise.create(:workout_id => @workout.id, :name => exercise[0], :weight => exercise[1])
+                #     end
+                # end
+                redirect to "/log/#{@workout.id}"
+            else
+                redirect to '/'
+            end
+        else
+            redirect to '/login'
+        end
+
     end
 
     delete '/log/:id' do
         #DELETE WORKOUT
         #find character
         #@workout.destroy
+        #@workout.clear
     end
 
+    private
+
+    def find_log
+        @workout = Workout.find_by(:id => params[:id])
+    end
+
+    def exercise_names
+        params[:workout][:exercise][:name].reject{ |name| name.empty?}
+    end
+
+    def exercise_weights
+        params[:workout][:exercise][:weight].reject{ |weight| weight.empty?}
+    end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-    has_many :workouts
+    has_many :workouts, dependent: :destroy
     
     has_secure_password
 
